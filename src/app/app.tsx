65f5ee8718d76a5b3af411a7cbc86dbea7f545b3
Shantanu Swami <shantanuswami@Shantanus-MacBook-Air.local>
2025-10-05 18:18:48 +0530

    fix: remove unused APP_NAME import

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { APP_NAME } from "~/lib/constants";
 
 // note: dynamic import is required for components that use the Frame SDK
 const BaseNinjaGame = dynamic(() => import("~/components/game/BaseNinjaGame").then(mod => ({ default: mod.BaseNinjaGame })), {
